eb25480d438394bb3de3ef7a6d27d5b1bea7e89e
3810ABHINAV <80142530+3810ABHINAV@users.noreply.github.com>
2021-08-14 10:19:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ noseY=0;
 
 function preload() {
   clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
-  
 }
-
 function setup() {
   canvas = createCanvas(400, 400);
   canvas.center();
@@ -29,13 +27,21 @@ function gotPoses(results)
     console.log(results);
     noseX = results[0].pose.nose.x-15;
     noseY = results[0].pose.nose.y-15;
- 
+  
+    //console.log("nose x="+ poseX);
+    //console.log("nose y="+ poseY);
+
   }
 }
 
 function draw() {
   image(video, 0, 0, 400, 400);
+  //code for red dot
+  //fill(255,0,0);
+  //stroke(255,0,0);
+  //circle(noseX,noseY,20);
   image(clown_nose, noseX, noseY, 30, 30);
+    
 }
 
 function take_snapshot(){    
